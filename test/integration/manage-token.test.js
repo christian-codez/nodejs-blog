b954d01f67b3264a2cecf12522990008dfabeb0b
Christian Nwachukwu <nwachukwu16@gmail.com>
2020-02-11 07:19:51 -0500

    Cleaned up the manage token test

--- test/integration/manage-token.test.js
+++ test/integration/manage-token.test.js
@@ -1,37 +1,10 @@
 const { User } = require("../../models/user");
 const helpers = require("../../helpers/manage-tokens");
 
-
-let mockUser;
-
-const createUser = async() => {
-    const user = new User(mockUser);
-    return await user.save();
-}
-
 describe('manage-tokens: ', () => {
 
-    beforeEach(() => {
-        mockUser = {
-            "name": "John Doe",
-            "email": "test1@gmail.com",
-            "password": "12345",
-            "gender": "male",
-            "country": "USA",
-            "age": "28",
-            "bio": "This is a sample bio",
-            "stacks": ["python", "nodejs"]
-        }
-    });
-
-    afterEach(async() => {
-        //await User.deleteMany({});
-    });
-
-
-
     it('return false if invalid user object was passed', () => {
-        const res = helpers.generateToken({ "name": "" });
+        const res = helpers.generateToken({});
         expect(res).toBeFalsy();
     });
 
@@ -47,6 +20,7 @@ describe('manage-tokens: ', () => {
             "stacks": ["python", "nodejs"]
         });
         user.save().then(data => user = data)
+
         const res = helpers.generateToken(user);
         expect(res).toBeTruthy();
     });
