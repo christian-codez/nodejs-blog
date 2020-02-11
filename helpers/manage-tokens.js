c3016ebe1b5dc283e31ea8e98af9848a11bed1f5
Christian Nwachukwu <nwachukwu16@gmail.com>
2020-02-11 07:15:12 -0500

    Wrote test for the token gen funtion

--- helpers/manage-tokens.js
+++ helpers/manage-tokens.js
@@ -1,15 +1,23 @@
 var jwt = require('jsonwebtoken');
 
 const generateToken = (user) => {
-    const userSignature = {
-        email: user.email,
-        name: user.name,
-        id: user._id,
-        role: user.role
-    };
+    try {
 
-    const key = process.env.jwtprivatekey;
-    return jwt.sign(userSignature, key);
+        if (!user.email && !user.name && !user._id && !user.role) return false;
+
+        const userSignature = {
+            email: user.email,
+            name: user.name,
+            id: user._id,
+            role: user.role
+        };
+
+        const key = process.env.jwtprivatekey;
+        return jwt.sign(userSignature, key);
+
+    } catch (error) {
+        return false;
+    }
 }
 
 const verifyToken = (token) => {
---
+++ test/integration/manage-token.test.js
@@ -0,0 +1,82 @@
+const { User } = require("../../models/user");
+const helpers = require("../../helpers/manage-tokens");
+
+
+let mockUser;
+
+const createUser = async() => {
+    const user = new User(mockUser);
+    return await user.save();
+}
+
+describe('manage-tokens: ', () => {
+
+    beforeEach(() => {
+        mockUser = {
+            "name": "John Doe",
+            "email": "test1@gmail.com",
+            "password": "12345",
+            "gender": "male",
+            "country": "USA",
+            "age": "28",
+            "bio": "This is a sample bio",
+            "stacks": ["python", "nodejs"]
+        }
+    });
+
+    afterEach(async() => {
+        //await User.deleteMany({});
+    });
+
+
+
+    it('return false if invalid user object was passed', () => {
+        const res = helpers.generateToken({ "name": "" });
+        expect(res).toBeFalsy();
+    });
+
+    it('return true if a valid user object was passed', () => {
+        let user = new User({
+            "name": "John Doe",
+            "email": "test1@gmail.com",
+            "password": "12345",
+            "gender": "male",
+            "country": "USA",
+            "age": "28",
+            "bio": "This is a sample bio",
+            "stacks": ["python", "nodejs"]
+        });
+        user.save().then(data => user = data)
+        console.log(res)
+        expect(res).toBeTruthy();
+    });
+});
+
+
+
+// beforeEach(() => {
+//     mockUser = {
+//         "name": "John Doe",
+//         "email": "test1@gmail.com",
+//         "password": "12345",
+//         "gender": "male",
+//         "country": "USA",
+//         "age": "28",
+//         "bio": "This is a sample bio",
+//         "stacks": ["python", "nodejs"]
+//     }
+// });
+
+
+// afterEach(async() => {
+//     await User.deleteMany({});
+// });
+
+
+// describe('Creating Token: ', () => {
+//     it('should return false if an invalide user object was passed', async() => {
+//         const user = await createUser();
+
+//         console.log(user)
+//     });
+// });
